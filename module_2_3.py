8179bd7c1b3fe1f2a2ec28a4426efe053d491840
Alexander <veber21@rambler.ru>
2024-07-01 09:36:31 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,9 +1,9 @@
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 start = 0  # переменная для индекса списка
 while start < len(my_list):
-    if my_list[start] < 0:
+    if my_list[start] < 0:  # завершаем вывод если элемент меньше нуля
         break
-    elif my_list[start] == 0:
+    elif my_list[start] == 0:  # пропускаем элемент, если равен нулю
         start = start + 1
         continue
     else:
